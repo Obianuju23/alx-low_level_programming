e7d5f35dfaf2afaab4f2a5dd5d079c169505375e
Obianuju23 <obianujunmoh@gmail.com>
2022-12-09 14:04:38 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * main - returns alphabets both except e and q
+ *main - returns alphabets both except e and q
  *
- * Returns: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 
 int main(void)
