fae4688cc6768194e0892497b555550ca36752b0
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 13:38:49 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - is a function that checks for lowercase character
  * @c: is the char value to be compared with lower case alphabet
- * Return: returns 1 if successful or 9 if otherwise
+ * Return: returns 1 if successful or 0 if otherwise
  */
 
 int _islower(int c)
