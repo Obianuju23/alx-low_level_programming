264a9eaddc8046adef3bc4f936de7d5c04cb9bbb
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 10:58:06 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ * main - print_alphabet_x10 - print alphabet a-z ten times
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet_x10(void);
+
+{
+	int y;
+	char x;
+
+	for (y = 0; y < 10; y++);
+	
+	{
+		for (x ='a'; x <='z'; x++);
+		{
+			_putchar x;
+		}
+
+		_putchar('\n');
+
+	}
+}
+
