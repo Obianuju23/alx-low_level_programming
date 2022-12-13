8d0dcd20410c76402c88bd9e31a27d37caad4403
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 15:09:35 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,15 @@
 
 /**
  * _isalpha - function that check if c is letter either lowercase or uppercase
- * @c: unsigned char value to be compared with ASCII letters
+ * @c: unsigned char value to be compared with ASCII values
  * Return: returns 1 if c is lowercase or uppercase otherwise returns 0
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
 
-			return (1);
-		else
-			return (0);
-	
 }
