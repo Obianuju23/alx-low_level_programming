410efd4f789e10a297ac72b8a326a3fd1f28a7c8
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 11:07:49 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,15 +15,14 @@ void print_alphabet_x10(void);
 	char x;
 
 	for (y = 0; y < 10; y++);
-	
 	{
-		for (x ='a'; x <='z'; x++);
+		for (x = 'a'; x <= 'z'; x++);
 		{
 			_putchar x;
 		}
 
 		_putchar('\n');
-
 	}
+
 }
 
