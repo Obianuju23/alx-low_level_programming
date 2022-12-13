5e48a01651394c57aa4f97fb3023a52a8ba2081c
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 11:51:53 -0800

    10 x alphabets

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -19,3 +19,4 @@ int main(void)
 	    print_alphabet_x10();
 
 	        return (0);
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (success)
  *
- */
+ i*/
 
 void print_alphabet_x10(void)
 
@@ -21,7 +21,7 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar('\n');
-		return (0);
+	
 	}
 
 }
