786ecfaa139e70877e348c46f154c92667e2d042
Obianuju23 <obianujunmoh@gmail.com>
2022-12-13 18:25:10 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * Add - entry point for addition
+ * @a: one of the values to be added
+ * @b: The second integer to be added
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
