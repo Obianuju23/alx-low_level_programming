cb5c3881f56d378a7436494a2bf96abaa256415d
Obianuju23 <obianujunmoh@gmail.com>
2022-12-21 17:09:51 -0800

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * cap_string - capitalizes every first letter of a word in a string
  * separators of words are: space, tabulation
@@ -12,7 +14,7 @@ char *cap_string(char *s)
 	int count;
 
 	count = 0;
-  while (s[count] != '\0')
+	while (s[count] != '\0')
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
