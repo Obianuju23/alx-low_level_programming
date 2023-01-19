0156a8aff2a4e92ad70fba6929539f0e1f87b198
Obianuju23 <obianujunmoh@gmail.com>
2023-01-18 17:46:26 -0800

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
-*void print_name - a function that prints a name
+*print_name - a function that prints a name
 *@name: The name to be printed
 *@f: A pointer to the function
 *Return: No return cause we've declared VOID as R_TYPE
