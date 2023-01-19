308c4570fdd847b5a9a48438dae27346c952f813
Obianuju23 <obianujunmoh@gmail.com>
2023-01-18 17:43:41 -0800

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+*void print_name - a function that prints a name
+*@name: The name to be printed
+*@f: A pointer to the function
+*Return: No return cause we've declared VOID as R_TYPE
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)/*if any is NULL */
+	return;
+		f(name);
+}
