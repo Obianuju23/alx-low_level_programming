2c8d8cbd9c17d3eafb7ddf9cc9765b366030bebc
Obianuju23 <obianujunmoh@gmail.com>
2023-01-19 19:07:47 -0800

    function that returns the sum of all its parameters

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,14 @@
+ #ifndef "VARIADIC_FUNCTION_H"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
+}
+#endif
