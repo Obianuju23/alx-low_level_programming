dd833492da9dff9f5b7880a3d13db4db2158bcc1
Obianuju23 <obianujunmoh@gmail.com>
2023-01-19 20:37:27 -0800

    function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,6 @@
 *print_strings - function that prints strings
 *@separator: pointer parameter
 *@n: parameter
-*Return: nil or ignore
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
