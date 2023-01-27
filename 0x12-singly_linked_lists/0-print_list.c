3e3d69b7882a950eeb8fa1cabb9e5e3ac384d2a8
Obianuju23 <obianujunmoh@gmail.com>
2023-01-27 09:02:53 -0800

    function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - prints all the elements of a list
  * @h: pointer to the struct containing the elements
  * struct is defined in list.h
  * Return: returns the number of element in the list
- *
- * Description - if NULL is passed to the print list function,
- * nothing is returned, else, the while loop prints the data in the node
- * provided its pointer part is not null.
- * it repeats loop until h meets the node that ends
- * the linked list with pointer part being null
  */
 size_t print_list(const list_t *h)
 {
