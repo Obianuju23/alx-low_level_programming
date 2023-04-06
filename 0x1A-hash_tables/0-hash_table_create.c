db3c05078685647f793cb27eb3f537180db432a8
Obianuju23 <obianujunmoh@gmail.com>
2023-04-06 13:36:46 -0700

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,4 @@
-#include "hash_tables.h"
+i#include "hash_tables.h"
 
 /**
  * hash_table_create - This is a function that creates a hash table
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
       /* Allocate memory space for the new hash table */
 	new_hash_table = malloc(sizeof(hash_table_t));
+
 	if (new_hash_table == NULL)
 		return (NULL);
 
@@ -21,14 +22,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
     /* Allocate memory space for the array of hash nodes */
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (new_hash_table->array == NULL)
 		return (NULL);
 	
 	i = 0;
 	while (i < size)
 	{
-	new_hash_table->array[i] = NULL;
-	i++;
+		new_hash_table->array[i] = NULL;
+		i++;
 	}
 	return (new_hash_table);
 }
