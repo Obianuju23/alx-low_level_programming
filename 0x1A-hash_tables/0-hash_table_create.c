1c84d6d155f331a1af8f9940d6cc0ced3f275dd2
Obianuju23 <obianujunmoh@gmail.com>
2023-04-06 12:46:55 -0700

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,35 +3,34 @@
 /**
  * hash_table_create - This is a function that creates a hash table
  * @size: the size of the array
- * Return: A pointer to the hash table newly created, 
+ * Return: A pointer to the hash table newly created,
  * or NULL incase of error
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_hash_table;
+	hash_table_t *new_hash_table;
 	unsigned long int i;
 
-    /* Allocate memory space for the hash table */
-    new_hash_table = malloc(sizeof(hash_table_t));
-    if (new_hash_table == NULL)
-        return NULL;
-    
-    /* Set the size of the new hash table */
-    new_hash_table->size = size;
+      /* Allocate memory space for the hash table */
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+		return (NULL);
+
+     /* Set the size of the new hash table */
+	new_hash_table->size = size;
 
     /* Allocate memory space for the array of hash nodes */
-    new_hash_table->array = calloc(size, sizeof(hash_node_t *));
-    if (new_hash_table->array == NULL)
-    {
-        free(new_hash_table);
-        return NULL;
-    }
-    i = 0;
-    while (i < size)
-    {
-    new_hash_table->array[i] = NULL;
-    i++;
-    }
-        
-    return new_hash_table;
+	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+	new_hash_table->array[i] = NULL;
+	i++;
+	}
+	return (new_hash_table);
 }
